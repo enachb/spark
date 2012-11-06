e21a29d9fee82ff0a62d97e03129978559217279
Imran Rashid <imran@quantifind.com>
2012-11-06 14:44:07 -0800

    no NPE if mapStatus is already null

--- core/src/main/scala/spark/MapOutputTracker.scala
+++ core/src/main/scala/spark/MapOutputTracker.scala
@@ -110,7 +110,10 @@ private[spark] class MapOutputTracker(actorSystem: ActorSystem, isMaster: Boolea
     var array = mapStatuses.get(shuffleId)
     if (array != null) {
       array.synchronized {
-        if (array(mapId).address == bmAddress) {
+        if (array(mapId) == null) {
+          logInfo("mapSustus is null for mapId: " + mapId)
+        }
+        else if (array(mapId).address == bmAddress) {
           array(mapId) = null
         }
       }
