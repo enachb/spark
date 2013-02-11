7ab97bbbf0bc06df745d85a72c3288ced3c7bbca
Imran Rashid <imran@quantifind.com>
2013-02-11 10:00:13 -0800

    add timing around parts of executor

--- core/src/main/scala/spark/executor/TaskMetrics.scala
+++ core/src/main/scala/spark/executor/TaskMetrics.scala
@@ -8,12 +8,24 @@ case class TaskMetrics(
   val localBlocksFetched: Option[Int],
   val remoteFetchWaitTime: Option[Long],
   val remoteBytesRead: Option[Long],
-  val shuffleBytesWritten: Option[Long]
-)
+  val shuffleBytesWritten: Option[Long],
+  executorDeserializeTime: Int,
+  executorRunTime:Int
+) {
+
+}
 
 object TaskMetrics {
-  private[spark] def apply(task: Task[_]) : TaskMetrics = {
-    TaskMetrics(task.totalBlocksFetched, task.remoteBlocksFetched, task.localBlocksFetched,
-      task.remoteFetchWaitTime, task.remoteReadBytes, task.shuffleBytesWritten)
+  private[spark] def apply(task: Task[_], executorStart: Long, taskStart: Long, taskFinish: Long) : TaskMetrics = {
+    TaskMetrics(
+      task.totalBlocksFetched,
+      task.remoteBlocksFetched,
+      task.localBlocksFetched,
+      task.remoteFetchWaitTime,
+      task.remoteReadBytes,
+      task.shuffleBytesWritten,
+      (taskStart - executorStart).toInt,
+      (taskFinish - taskStart).toInt
+    )
   }
 }
